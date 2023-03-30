a43a91c602d599585753311c0755d271e939fca0
Baiju Abraham <79013601+baijuka@users.noreply.github.com>
2023-03-30 22:56:59 +0100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,12 +6,12 @@ const About = () => {
       <h1 className='text-6xl mb-4'>Github Finder</h1>
       <p className='mb-4 text-2xl font-light'>
         A React app to search GitHub profiles and see profile details. This
-        project is part of the
+        project is developed by Baiju as part of Udemy Course 
         <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
           {' '}
           React Front To Back
         </a>{' '}
-        Udemy course by
+        by
         <strong>
           <a href='https://traversymedia.com'> Brad Traversy</a>
         </strong>
